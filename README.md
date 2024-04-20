f955a9822b5c751f8665a1a9cb1492ec3b83f3f0
Anderson Dias da Silva <olhardeveloper@gmail.com>
2024-04-19 21:07:18 -0300

    subindo projeto

---
+++ README.md
@@ -0,0 +1,3 @@
+App em  http://seu_servidor:8080
+Prometheus em http://seu_servidor:9090
+Grafana em http://seu_servidor:3000
---
+++ main.tf
@@ -0,0 +1,72 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "dev-server2" {
+  ami           = "ami-080e1f13689e07408"
+  instance_type = "t2.micro"
+  key_name = "ec2-key-dev"
+
+  vpc_security_group_ids = [
+    "grp_sec_dev"
+  ]
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      if ! grep -q "${aws_instance.dev-server2.public_dns} ansible_ssh_user=ubuntu ansible_ssh_private_key_file=./ssh/my_key" hosts; then
+        echo -e "\n${aws_instance.dev-server2.public_dns} ansible_ssh_user=ubuntu ansible_ssh_private_key_file=./ssh/my_key" >> hosts
+      fi
+      sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}' hosts
+    EOT
+  }
+
+  depends_on = [aws_instance.dev-server2]
+}
+
+resource "aws_key_pair" "key-Dev" {
+    key_name = "ec2-key-dev"
+    public_key = file("./ssh/my_key.pub")
+}
+
+resource "aws_security_group" "grp_sec_dev" {
+  name = "grp_sec_dev"
+  description = "Ambiente Dev"
+  ingress{
+      cidr_blocks = [ "0.0.0.0/0" ]
+      ipv6_cidr_blocks = [ "::/0" ]
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+  }
+  egress{
+      cidr_blocks = [ "0.0.0.0/0" ]
+      ipv6_cidr_blocks = [ "::/0" ]
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+  }
+  tags = {
+    Name = "grp_sec_dev"
+  }
+}
+
+output "ip_publico" {
+  value = aws_instance.dev-server2.public_ip
+}
+
+output "public_dns" {
+  value = aws_instance.dev-server2.public_dns
+}
+
